a8b6171d7c45f8c81a394e632dbc4805c2860e21
lkjj-lkjj <20301105@bjtu.edu.cn>
2022-12-01 18:02:56 +0800

    20301105 李可佳

--- user/target/riscv64gc-unknown-none-elf/release/my_rs.d
+++
@@ -1,1 +0,0 @@
-/mnt/user/target/riscv64gc-unknown-none-elf/release/my_rs: /mnt/user/src/bin/my_rs.rs /mnt/user/src/console.rs /mnt/user/src/lang_items.rs /mnt/user/src/lib.rs /mnt/user/src/syscall.rs
