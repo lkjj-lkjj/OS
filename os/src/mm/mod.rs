d8c3cc0799f1c3ee6a7afef2e4712971515a0960
lkjj-lkjj <20301105@bjtu.edu.cn>
2022-12-09 22:20:59 +0800

    20301105 李可佳

--- os/src/mm/mod.rs
+++ os/src/mm/mod.rs
@@ -1,11 +1,19 @@
 mod heap_allocator;
 mod page_table;
+mod address;
+mod frame_allocator;
 
 use page_table::{PTEFlags};
+use address::{VPNRange, StepByOne};
 
 pub use page_table::{PageTableEntry};
+pub use address::{PhysAddr, VirtAddr, PhysPageNum, VirtPageNum};
+pub use frame_allocator::{FrameTracker, frame_alloc};
 
 pub fn init() {
     heap_allocator::init_heap();
     heap_allocator::heap_test();
+    frame_allocator::init_frame_allocator();
+    frame_allocator::frame_allocator_test();
 }
+
