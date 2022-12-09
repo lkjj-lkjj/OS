5ee9bfb5154944eae3df71ff1eec5065de109445
lkjj-lkjj <20301105@bjtu.edu.cn>
2022-12-09 23:28:49 +0800

    20301105 李可佳

--- os/src/config.rs
+++ os/src/config.rs
@@ -8,3 +8,5 @@ pub const CLOCK_FREQ: usize = 12500000;
 pub const MEMORY_END: usize = 0x80800000;
 pub const PAGE_SIZE: usize = 0x1000;
 pub const PAGE_SIZE_BITS: usize = 0xc;
+pub const TRAMPOLINE: usize = usize::MAX - PAGE_SIZE + 1;
+pub const TRAP_CONTEXT: usize = TRAMPOLINE - PAGE_SIZE;
