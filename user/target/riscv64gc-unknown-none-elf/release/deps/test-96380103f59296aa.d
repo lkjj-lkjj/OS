41a34da649f8b310ea27f05660332eaaadf38025
lkjj-lkjj <20301105@bjtu.edu.cn>
2022-12-16 01:31:42 +0800

    20301105 李可佳

---
+++ user/target/riscv64gc-unknown-none-elf/release/deps/test-96380103f59296aa.d
@@ -0,0 +1,5 @@
+/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/test-96380103f59296aa: src/bin/test.rs
+
+/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/test-96380103f59296aa.d: src/bin/test.rs
+
+src/bin/test.rs:
---
+++ user/target/riscv64gc-unknown-none-elf/release/test.d
@@ -0,0 +1,1 @@
+/mnt/user/target/riscv64gc-unknown-none-elf/release/test: /mnt/user/src/bin/test.rs /mnt/user/src/console.rs /mnt/user/src/lang_items.rs /mnt/user/src/lib.rs /mnt/user/src/syscall.rs
